61a1b0fc6638b0b0bc4afdc955de9de6b9a781b4
Alireza Rezaee <alireza-git-commits@rezaee.org>
2021-06-26 04:15:04 +0430

    Add Jekyll & customize theme

---
+++ assets/main.js
@@ -0,0 +1,12 @@
+﻿const englishNumbersToPersian = s => String(s).replace(/\d/g, d => '۰۱۲۳۴۵۶۷۸۹'[d]);
+
+$(document).ready(function () {
+    $('<div class="mt-5"></div>').css("height", $('#footer').outerHeight()).appendTo('body>.wrapper');
+    $('.js-tooltip').tooltip();
+    $('.js-gotop').click(function () {
+        $('html, body').animate({ scrollTop: 0 });
+    });
+    $('#js-nav-shortcuts-trigger').click(function () {
+        $(this).siblings('#js-nav-shortcuts').toggleClass('open').hide().fadeIn();
+    });
+});
